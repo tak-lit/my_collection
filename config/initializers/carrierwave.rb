da84494ad415e32168067ce2640f0b426899b22b
vagrant <vagrant@vagrant-centos63.(none)>
2014-08-15 06:45:13 +0100

    carrierwave.rbを変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAIQOXPM3PYHVP6QOQ',
-    :aws_secret_access_key  => '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',
+    :aws_secret_access_key  => '4x7YMdBeyNsd5X/rNE/xECJasYyKMxA1yTcItHoe',
     :region                 => 'ap-northeast-1'
   }
-
-  config.fog_directory = 'kobashi'
-
+  config.fog_directory = 'litmycollection'
 end
